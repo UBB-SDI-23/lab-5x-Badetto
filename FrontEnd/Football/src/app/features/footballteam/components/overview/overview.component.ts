708a2f75387f61de62c35f721bb8457ec491b557
Badu Catalin-Ioachim <catalinbaduca@gmail.com>
2023-04-09 10:46:29 +0300

    deleted env

--- FrontEnd/Football/src/app/features/footballteam/components/overview/overview.component.ts
+++ FrontEnd/Football/src/app/features/footballteam/components/overview/overview.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { FootballTeam } from '../../models/footballteams.models';
 import { ApiService } from 'src/app/common/services/services.api.service.service';
 import { Router } from '@angular/router';
 import { NrPlayers } from '../../models/nrplayers.models';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent implements OnInit{
+  @ViewChild(MatTable) table: MatTable<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ['id', 'name', 'nickname', 'value', 'nrplayers']
   footballteams: FootballTeam[] = [];
   numberplayers: Number[] = [];
+  dataSource = new MatTableDataSource<FootballTeam>(this.footballteams);
 
-  constructor(private apiSvc: ApiService,
+  constructor(private apiSvc: ApiService, 
     private router: Router) {}
   ngOnInit(): void {
     this.apiSvc.getFootballTeams().subscribe((result: FootballTeam[]) => {
@@ -23,7 +29,12 @@ export class OverviewComponent implements OnInit {
           this.footballteams[i]["nrplayers"] = result2.nr_players
         })
       }
+      this.table.renderRows();   
     });
+   
+    this.dataSource = new MatTableDataSource<FootballTeam>(this.footballteams);
+    this.dataSource.paginator = this.paginator;
+    
   }
   goToDetails(courseId: number){
     this.router.navigateByUrl(`football-team/${courseId}`)
@@ -33,5 +44,6 @@ export class OverviewComponent implements OnInit {
   }
   sortName(){
     this.footballteams.sort((a, b) => a.name.localeCompare(b.name))
+    this.table.renderRows();
   }
 }
